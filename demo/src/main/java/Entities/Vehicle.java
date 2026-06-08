167c83ea74a2d869ab0a0e0fb11d782d42d3bdeb
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 10:22:38 +0400

    adding getter and setter methods

--- demo/src/main/java/Entities/Vehicle.java
+++ demo/src/main/java/Entities/Vehicle.java
@@ -10,4 +10,28 @@ public class Vehicle {
         this.vehicleModel = vehicleModel;
         this.rentalPricePerDay = rentalPricePerDay;
     }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    public void setRentalPricePerDay(double rentalPricePerDay) {
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
 }

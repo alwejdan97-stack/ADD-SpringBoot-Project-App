8ab14e51cb9117e4e699b14f3632bdac6a2516d9
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 09:46:43 +0400

    adding employees

---
+++ demo/src/main/java/Services/EmployeeMain.java
@@ -0,0 +1,17 @@
+package Services;
+
+import Entities.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeMain {
+    public static Map<String,Employee> employeeMap=new HashMap<>();
+    public static void main(String[] args){
+        employeeMap.put("E1",new Employee(101,"Ahmed","IT"));
+        employeeMap.put("E2",new Employee(202,"Ali","ABI"));
+        employeeMap.put("E3",new Employee(303,"Omar","English"));
+        employeeMap.put("E4",new Employee(404,"Sara","Islamics"));
+
+
+}

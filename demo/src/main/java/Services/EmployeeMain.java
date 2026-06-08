a5682d737e3835a5d02b796fe9ed8ae73a32ac28
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 09:58:12 +0400

    try to add new employees

--- demo/src/main/java/Services/EmployeeMain.java
+++ demo/src/main/java/Services/EmployeeMain.java
@@ -7,11 +7,25 @@ import java.util.Map;
 
 public class EmployeeMain {
     public static Map<String,Employee> employeeMap=new HashMap<>();
-    public static void main(String[] args){
-        employeeMap.put("E1",new Employee(101,"Ahmed","IT"));
-        employeeMap.put("E2",new Employee(202,"Ali","ABI"));
-        employeeMap.put("E3",new Employee(303,"Omar","English"));
-        employeeMap.put("E4",new Employee(404,"Sara","Islamics"));
+    public static void main(String[] args) {
+        employeeMap.put("E1", new Employee(101, "Ahmed", "IT"));
+        employeeMap.put("E2", new Employee(202, "Ali", "ABI"));
+        employeeMap.put("E3", new Employee(303, "Omar", "English"));
+        employeeMap.put("E4", new Employee(404, "Sara", "Islamics"));
+
+        System.out.println("*** Display Existing Employees ***");
+        for(Employee e:employeeMap.values()){
+            System.out.println("Employee ID: "+e.getEmployeeId()+" | Employee Name: "+ e.getEmployeeName()+ " | Department: "+e.getDepartment());
+
+        }
 
 
+        Employee newEmployee=new Employee(505,"Wejdan","IT");
+        if(employeeMap.containsKey(newEmployee.getEmployeeId())){
+            System.out.println("Employee ID Is Already Exist, Adding NOT Perform...");
+        }else{
+            System.out.println("New Employee Added: \n"+
+                    "Employee ID: "+newEmployee.getEmployeeId()+" | Employee Name: "+ newEmployee.getEmployeeName()+ " | Department: "+newEmployee.getDepartment());
+        }
+    }
 }

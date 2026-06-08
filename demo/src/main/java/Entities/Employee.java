7ff30718222c9ca91fae5d3463abc955754764ce
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 09:39:50 +0400

    adding getter and setter methods in Employee class

--- demo/src/main/java/Entities/Employee.java
+++ demo/src/main/java/Entities/Employee.java
@@ -10,4 +10,28 @@ public class Employee {
         this.employeeName = employeeName;
         this.department = department;
     }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }

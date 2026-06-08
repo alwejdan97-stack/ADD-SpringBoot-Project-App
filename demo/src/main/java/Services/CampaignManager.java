e350b738eedc4e96800ef501e477c7d93111175e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 10:56:03 +0400

    create addCampaign(Campaign newCampaign) methods for accepting new Campaing

--- demo/src/main/java/Services/CampaignManager.java
+++ demo/src/main/java/Services/CampaignManager.java
@@ -11,6 +11,16 @@ public class CampaignManager {
         campaignMap.put(101,new Campaign(101, "Summer Sale", "Instagram", 500.00));
         campaignMap.put(202, new Campaign(202, "Black Friday", "Google Ads", 2500.50));
         campaignMap.put(303, new Campaign(303, "Brand Awareness", "TikTok", 1200.00));
-        campaignMap.put(404, new Campaign(404, "App Installs", "Facebook", 850.75)):
+        campaignMap.put(404, new Campaign(404, "App Installs", "Facebook", 850.75));
+    }
+
+    public static void addCampaign(Campaign newCampaign){
+        if(campaignMap.containsKey(newCampaign.getCampaignId())){
+            System.out.println("Campaign ID Is Already Exist, Adding NOT Perform...");
+        }else{
+            campaignMap.put(newCampaign.getCampaignId(), newCampaign);
+            System.out.println("New Campaign Added Successfully... ");
+            System.out.println("Campaign ID: "+newCampaign.getCampaignId()+" | Campaign Name: "+ newCampaign.getCampaignName()+ " | Platform: "+newCampaign.getPlatform()+" | Budget:"+newCampaign.getBudget());
+        }
     }
 }

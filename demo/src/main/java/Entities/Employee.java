4ab20e3f14cd753a5a78c85e9b2a4674f913d30e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-08 09:38:19 +0400

    creating costructor in Employee class

--- demo/src/main/java/Entities/Employee.java
+++ demo/src/main/java/Entities/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     private int employeeId;
     private String employeeName;
     private String department;
+
+    public Employee(int employeeId, String employeeName, String department) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+    }
 }

bd64213680102379bca044cd20775d5c32863ed1
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-09 10:39:41 +0400

    adding reqired annotation for SQL

--- demo/src/main/java/Entities/Employee.java
+++ demo/src/main/java/Entities/Employee.java
@@ -1,53 +1,33 @@
 package Entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
 import org.jspecify.annotations.NonNull;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name="Employee")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int employeeId;
-
     private String employeeName;
     private String department;
+
+    @OneToMany(mappedBy = "employee")
+    private List<Vehicle> vehicleList;
 
    /* public Employee(int employeeId, String employeeName, String department) {
         this.employeeId = employeeId;
         this.employeeName = employeeName;
         this.department = department;
     }*/
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
--- demo/src/main/java/Entities/Vehicle.java
+++ demo/src/main/java/Entities/Vehicle.java
@@ -1,37 +1,25 @@
 package Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "Vehicle")
 public class Vehicle {
     private int vehicleId;
     private String vehicleModel;
     private double rentalPricePerDay;
 
-    public Vehicle(int vehicleId, String vehicleModel, double rentalPricePerDay) {
+    /*public Vehicle(int vehicleId, String vehicleModel, double rentalPricePerDay) {
         this.vehicleId = vehicleId;
         this.vehicleModel = vehicleModel;
         this.rentalPricePerDay = rentalPricePerDay;
-    }
-
-    public int getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(int vehicleId) {
-        this.vehicleId = vehicleId;
-    }
-
-    public String getVehicleModel() {
-        return vehicleModel;
-    }
-
-    public void setVehicleModel(String vehicleModel) {
-        this.vehicleModel = vehicleModel;
-    }
-
-    public double getRentalPricePerDay() {
-        return rentalPricePerDay;
-    }
-
-    public void setRentalPricePerDay(double rentalPricePerDay) {
-        this.rentalPricePerDay = rentalPricePerDay;
-    }
+    }*/
 }

16c9b124f5fbeff8d352e673b0f503ee21d0781a
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-09 10:53:08 +0400

    adding reqired annotation for SQL

--- demo/src/main/java/Entities/Campaign.java
+++ demo/src/main/java/Entities/Campaign.java
@@ -3,6 +3,8 @@ package Entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Collection;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,9 +18,13 @@ public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int campaignId;
+
     private String campaignName;
     private String platform;
     private double budget;
+
+    @OneToMany(mappedBy = "campaign")
+    private Collection<Vehicle> vehicles;
 
     /*public Campaign(int campaignId, String campaignName, String platform, double budget) {
         this.campaignId = campaignId;

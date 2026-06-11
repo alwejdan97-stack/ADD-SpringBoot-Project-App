2315277c293ba0bc185594d15c121651541c4dc5
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-11 12:15:27 +0400

    add new CRUD codes

--- demo/src/main/java/Task8App/demo/Entities/Campaign.java
+++ demo/src/main/java/Task8App/demo/Entities/Campaign.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Campaign {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int campaignId;
 
     private String campaignName;
